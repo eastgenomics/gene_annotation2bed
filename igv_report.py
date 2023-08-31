53b099ff9ed4988cd52d6e9efcc3260a67ec5839
RSWilson1 <robert.s.wilson1996@gmail.com>
2023-08-31 14:51:47 +0100

    Added docstring and commented out lines for testing.

--- igv_report.py
+++ igv_report.py
@@ -1,29 +1,45 @@
 import subprocess
 
 def create_igv_report(bed_file, genome, info_columns, title, output_file):
+    """
+    Create an IGV report from a bed file.
+    Parameters
+    ----------
+    bed_file : str
+        file path to bed file.
+    genome : str
+        genome name. i.e hg19.
+    info_columns : list
+        list of column names to be displayed in the report.
+    title : str
+        title of the report.
+    output_file : str
+        file path to save the report.
 
-
+    Returns
+    -------
+    Prints the standard output and error of the subprocess.
+    Creates an IGV report.
+    """
     bed_based_cmd = [
         "create_report",
         bed_file,
         "--fasta", "/home/rswilson1/Documents/MSC_dissertation/MELT_example_data/hs37d5.fa",
         "--tracks", bed_file,
         "--track-config", "TracksConfig.json",
-        #"--genome", genome,
         "--title", title,
         "--output", output_file
     ]
 
     # tsv_based_cmd = [
     #     "create_report",
-    #     "/home/rswilson1/Documents/Programming_project/gene_annotation2bed/output_hg19_overlap_test5_edit.tsv",
-    #     "--flanking", "100",
+    #     "/home/rswilson1/Documents/Programming_project/gene_annotation2bed/output_hg19_overlap_test5_edit.maf",
     #     "--sequence", "1",
     #     "--begin", "2",
     #     "--end", "3",
     #     "--fasta", "/home/rswilson1/Documents/MSC_dissertation/MELT_example_data/hs37d5.fa",
     #     "--zero_based", "true",
-    #     "--info-columns", "chr", "start", "end", "annotation", "gene", # info_columns
+    #     #"--info-columns", "chr", "start", "end", "annotation", "gene", # info_columns
     #     "--tracks", bed_file,
     #     "--track-config", "TracksConfig.json",
     #     #"--genome", genome,
